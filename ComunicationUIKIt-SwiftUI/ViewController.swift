d1f6f6fcbb8fa8d03260413ce8d690309a652eae
Martons00 <raffaelemartone34@gmail.com>
2023-03-28 16:54:59 +0200

    Initial Commit

---
+++ ComunicationUIKIt-SwiftUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ComunicationUIKIt-SwiftUI
+//
+//  Created by Raffaele Martone on 28/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
